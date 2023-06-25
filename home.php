c875cdc296e4119bc6fdc984af8e68618f9ed714
Aatish124 <aatishk60@gmail.com>
2023-06-25 12:07:17 +0530

    added favicon :p

--- home.php
+++ home.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/png">
     <style>
         html,
         body {
--- verify.php
+++ verify.php
@@ -32,6 +32,7 @@ if (isset($_GET['email']) && isset($_GET['token'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/png">
     <style>
         html,
         body {
