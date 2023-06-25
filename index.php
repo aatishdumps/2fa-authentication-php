f911f3e39aa882c9d517e3a285275f68ad1c6a90
Aatish124 <aatishk60@gmail.com>
2023-06-25 11:38:20 +0530

    bug fixes

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                     <input type="hidden" name="action" value="login">
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your username">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email address">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
